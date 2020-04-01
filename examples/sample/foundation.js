4e2299886abe08cb69cef8399103d26139b4048b
Gabin Desserprit <desserprit.gabin@gmail.com>
2020-04-01 10:46:38 +0300

    Remove examples for now

--- examples/sample/foundation.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config({});
-const { Biota } = require("../../dist");
-
-(async () => {
-  const db = new Biota({ secret: process.env.FAUNA_DB_SECRET });
-  await db.foundation();
-  // .then(res => console.log(JSON.stringify(res, null, 2)))
-  // .catch(err => console.error(JSON.stringify(err, null, 2)));
-})();
