bb9f53ccc910cfa494776e2070865491bb30cd5e
Gabin Desserprit <desserprit.gabin@gmail.com>
2020-04-09 22:45:04 +0300

    Gotten rid of module-aliases *-*

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import moduleAlias from "module-alias";
-moduleAlias.addAlias("~", __dirname);
+// import moduleAlias from "module-alias";
+// moduleAlias.addAlias("~", __dirname);
 
 export { DB as Biota } from "./db";
 export { query as q } from "faunadb";
