8aaa766c0b5242cbe477df71337fe51e3f859262
Gabin Desserprit <desserprit.gabin@gmail.com>
2020-04-09 22:33:15 +0300

    Trying to fix module-alias mess

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import "module-alias/register";
+import moduleAlias from "module-alias";
+moduleAlias.addAlias("~", __dirname);
 
 export { DB as Biota } from "./db";
 export { query as q } from "faunadb";
